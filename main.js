b55aa245fe59a00fcdc7cad7e5362707e3fe23a8
tudemaha <gede_mahardika88@yahoo.co.id>
2024-03-17 17:23:17 +0800

    [add] create wa client and config

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
 require("dotenv").config();
 const mongoConnect = require("./src/pkg/mongo");
-const Payment = require('./src/model/Payment');
+const waClient = require("./src/pkg/whatsapp");
+const {
+  clientReadyHandler,
+  qrReadyHandler,
+  messageRegexHandler,
+  pingHandler,
+} = require("./src/controller/whatsapp-controller");
+
+const startMongo = async () => {
+  await mongoConnect();
+};
+
+const startWa = () => {
+  clientReadyHandler(waClient);
+  qrReadyHandler(waClient);
+  pingHandler(waClient);
+  messageRegexHandler({
+    regex: /^[Pp]+$/,
+    rep:
+      "Please use greetings when starting a chat." +
+      "\n" +
+      "(Tolong awali chat dengan salam.)" +
+      "\n\n" +
+      `_This message was sent by bot._`,
+    client: waClient,
+  });
+  waClient.initialize();
+};
+
+startMongo();
+startWa();
